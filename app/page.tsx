f05681295aefc0570a7c13a443548a467aadbc4d
DennisH18 <111347933+DennisH18@users.noreply.github.com>
2024-04-23 15:18:02 +0800

    issue with carousel

--- app/page.tsx
+++ app/page.tsx
@@ -15,38 +15,7 @@ import {
 } from "react-icons/ai";
 import { TypeAnimation } from "react-type-animation";
 import { TbSchool } from "react-icons/tb";
-import Autoplay from "embla-carousel-autoplay";
-import Carousel from "@/components/ui/carousel";
-
-
-interface CarouselItem {
-  id: number;
-  title: string;
-  desc: string;
-  images: string[];
-}
-
-
-const items: CarouselItem[] = [
-  {
-    id: 1,
-    title: "Card 1",
-    desc: "Description 1",
-    images: ["adi-1.jpg", "image1-2.jpg", "image1-3.jpg"],
-  },
-  {
-    id: 2,
-    title: "Card 2",
-    desc: "Description 2",
-    images: ["image2-1.jpg", "image2-2.jpg", "image2-3.jpg"],
-  },
-  {
-    id: 3,
-    title: "Card 3",
-    desc: "Description 3",
-    images: ["image3-1.jpg", "image3-2.jpg", "image3-3.jpg"],
-  },
-];
+import MusicPlayer from "./musicplayer";
 
 export default function Home() {
   const [selectedItem, setSelectedItem] = useState("introduction");
@@ -99,13 +68,12 @@ export default function Home() {
     setShowMore(!showMore);
   };
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedProject, setSelectedProjec] = useState(1);
 
-  const handleSlideChange = (currentItem: any, pageIndex: any) => {
-    setCurrentIndex(pageIndex);
+  const handleRadioChange = (id: any) => {
+    setSelectedItem(id);
+    document.body.classList.toggle("blue");
   };
-
-  const plugin = useRef(Autoplay({ delay: 2500, stopOnInteraction: true }));
 
   return (
     <div className="flex">
@@ -529,8 +497,80 @@ export default function Home() {
             </ol>
           </div>
         </div>
+        <div ref={contentRefs.projects} id="projects" className="min-h-screen">
+          <div className="w-[70%] ml-[10%] mt-20 relative">
+            <div className="flex items-center text-blue-500 text-2xl z-30 mb-4">
+              <h2 className="font-mono">Projects</h2>
+            </div>
+            {""}
+            <div ref={contentRefs.projects} id="projects" className="min-h-screen">
+  <div className="w-[70%] ml-[10%] mt-20 relative">
+    <div className="flex items-center text-blue-500 text-2xl z-30 mb-4">
+      <h2 className="font-mono">Projects</h2>
+    </div>
+    <div className="container">
+      <div className="cards">
+        <label className="card" htmlFor="item-1" id="song-1">
+          <img src="https://images.unsplash.com/photo-1530651788726-1dbf58eeef1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=882&q=80" alt="song" />
+        </label>
+        <label className="card" htmlFor="item-2" id="song-2">
+          <img src="https://images.unsplash.com/photo-1559386484-97dfc0e15539?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80" alt="song" />
+        </label>
+        <label className="card" htmlFor="item-3" id="song-3">
+          <img src="https://images.unsplash.com/photo-1533461502717-83546f485d24?ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt="song" />
+        </label>
+      </div>
+      <div className="player">
+        <div className="upper-part">
+          <div className="play-icon">
+            <svg width="20" height="20" fill="#2992dc" stroke="#2992dc" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="feather feather-play" viewBox="0 0 24 24">
+              <path d="M5 3l14 9-14 9V3z" />
+            </svg>
+          </div>
+          <div className="info-area">
+            <label className="song-info" id="song-info-1">
+              <div className="title">Bunker</div>
+              <div className="sub-line">
+                <div className="subtitle">Balthazar</div>
+                <div className="time">4.05</div>
+              </div>
+            </label>
+            <label className="song-info" id="song-info-2">
+              <div className="title">Words Remain</div>
+              <div className="sub-line">
+                <div className="subtitle">Moderator</div>
+                <div className="time">4.05</div>
+              </div>
+            </label>
+            <label className="song-info" id="song-info-3">
+              <div className="title">Falling Out</div>
+              <div className="sub-line">
+                <div className="subtitle">Otzeki</div>
+                <div className="time">4.05</div>
+              </div>
+            </label>
+          </div>
+        </div>
+        <div className="progress-bar">
+          <span className="progress"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-        
+
+
+          </div>
+        </div>
+
+        <div
+          ref={contentRefs.skills}
+          id="skills"
+          className="min-h-screen bg-purple-400"
+        >
+          Skills section content
+        </div>
       </div>
     </div>
   );
